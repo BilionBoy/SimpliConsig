726f7c97600a56af9f6a3600e4ac1cc3e44ddb84
Dhiogoandre <dhiogoandre26@gmail.com>
2025-08-03 13:55:40 -0400

    WIP: SimpliConsig

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module AguiarAssessoria
+module SimpliConsig
   class Application < Rails::Application # rubocop:disable Style/Documentation
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.2
